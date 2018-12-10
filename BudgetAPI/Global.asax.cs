54504092972afa8dcbb697ba2b560b5687df1662
Dylan Jackson <dj0759@gmail.com>
2018-12-09 18:57:21 -0700

    initialCommit

--- BudgetAPI/Global.asax.cs
+++ BudgetAPI/Global.asax.cs
@@ -13,11 +13,20 @@ namespace BudgetAPI
     {
         protected void Application_Start()
         {
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+                .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~/bin"));
+
         }
     }
 }
